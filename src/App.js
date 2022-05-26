4b0b09767decd6f6da6816e0fe032149d79377d0
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-26 06:49:58 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Signupdonor from './Components/Signupdonor';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signindonor from './Components/Signindonor';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Signupdonor/>}/>
+        <Route path="/signin" exact element={<Signindonor/>}/>
+
       </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Signindonor.js
+++ src/Components/Signindonor.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Signindonor = () => {
+   
+    var[username,setusername]=useState("")
+    var[password,setpassword]=useState("")
+    const subdata=()=>{
+        const data={"username":username,"passworod":password}
+        console.log(data)
+    }
   return (
-    <div>Signindonor</div>
+      
+    <div>
+        <div>
+<div className="container">
+    <div className="row div col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <div className="row g-3">
+           
+            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" className="form-label">Username</label>
+                <input onChange={(c)=>{setusername(c.target.value)}} type="text" className="form-control"/>
+            </div>
+            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" className="form-label">Password</label>
+               <input onChange={(c)=>{setpassword(c.target.value)}} type="password" name="" id="" class="form-control"/>
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button onClick={subdata} className="btn btn-success">REGISTER</button>
+                
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                
+             <Link to="/view">view donor</Link>
+
+            </div>
+            
+        </div>
+    </div>
+</div>
+</div>
+
+    </div>
   )
 }
 
